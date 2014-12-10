18022c0faa9c8f1eb0dd4e750e1a9d267a062cc8
edgard <edgard@localhost.localdomain>
2014-12-09 23:24:43 -0300

    Envio Hash

--- Lista_3/Questao_5.c
+++ Lista_3/Questao_5.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main() {
+typedef struct
+{
+    int matricula;
+    char sobrenome[20];
+    int anonascimento;
+}nodoaluno;
 
-    return (EXIT_SUCCESS);
+void main (){
+    int numalunos,i;
+    nodoaluno *nw;
+	 nw = (nodoaluno*)malloc(sizeof(nodoaluno));
+    printf("Digite a quantidade de alunos: ");
+    scanf("%i", &numalunos);
+		
+
+    
+    for (i=0; i < numalunos; i++)
+    {
+        printf("Digite o sobrenome do aluno: ");
+        scanf(" %s", &nw->sobrenome);
+        printf("Digite a matricula do aluno: ");
+        scanf(" %i", &nw->matricula);
+        printf("Digite o ano de nascimento do aluno: ");
+        scanf(" %i", &nw->anonascimento);
+        nw++;
+
+    }
+
+    nw = nw - numalunos;
+
+    for (i=0; i<numalunos; i++)
+    {
+        printf("Aluno %i:\n", i+1);
+        printf("Sobrenome: %s\n", (nw[i]).sobrenome);
+        printf("Matricula: %i\n", (nw[i]).matricula);
+        printf("Ano de nascimento: %i\n", (nw[i]).anonascimento);
+        printf("\n");
+    }
+
+    free (nw);
 }
-
